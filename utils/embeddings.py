7eb7ad0039b9f1ca6c577a161226b9abe067ebe8
Seelam-Mohith <seelammohith2222@gmail.com>
2026-04-25 12:00:00 +0530

    Added Embeddings

---
+++ utils/embeddings.py
@@ -0,0 +1,7 @@
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from config import GOOGLE_API_KEY
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="models/embedding-001",
+    google_api_key=GOOGLE_API_KEY
+)
