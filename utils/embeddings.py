4ed1812cecfdb210615ae9383d8665cdae61bb58
Seelam-Mohith <seelammohith2222@gmail.com>
2026-04-27 12:00:00 +0530

    Embeddings Model changed

--- utils/embeddings.py
+++ utils/embeddings.py
@@ -1,7 +1,5 @@
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from config import GOOGLE_API_KEY
+from langchain_huggingface import HuggingFaceEmbeddings
 
-embeddings = GoogleGenerativeAIEmbeddings(
-    model="models/embedding-001",
-    google_api_key=GOOGLE_API_KEY
+embeddings = HuggingFaceEmbeddings(
+    model_name="all-MiniLM-L6-v2"
 )
